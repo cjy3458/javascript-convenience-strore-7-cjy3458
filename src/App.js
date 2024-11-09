8ef4d0be0fb0c54a067b634c487ab9f5fd366f57
CJY <cjy3458@naver.com>
2024-11-10 01:58:43 +0900

    feat: 상품 정보 출력 기능 구현
    현재 재고 상태를 출력하는 기능 구현.
    상품명, 가격, 재고, 프로모션 정보를 보기 좋게 정렬하여 출력하는 기능 구현

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import Store from './models/Store.js';
+import InputView from './views/InputView.js';
+import OutputView from './views/OutputView.js';
 
 class App {
+  constructor() {
+    this.store = new Store();
+  }
+
   async run() {
-    const store = new Store();
-    store.initializeStore();
+    OutputView.printWelcomeMessage();
+    this.store.initializeStore();
+
+    OutputView.printProductList(this.store.products);
   }
 }
 
