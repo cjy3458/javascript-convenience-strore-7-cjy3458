c064095e71baee36efb234e582b55f485b6bd4b2
CJY <cjy3458@naver.com>
2024-11-10 01:56:01 +0900

    feat: 상품 및 프로모션 데이터 초기화 기능 구현

--- src/models/Store.js
+++ src/models/Store.js
@@ -1,5 +1,7 @@
 import fs from 'fs';
 import path from 'path';
+import Product from './Product.js';
+import Promotion from './Promotion.js';
 
 class Store {
   constructor() {
@@ -15,13 +17,43 @@ class Store {
   loadProducts() {
     const filePath = path.resolve('public/products.md');
     const data = fs.readFileSync(filePath, 'utf-8').trim();
-    console.log(data);
+    const [header, ...rows] = data.split('\n');
+    return rows.map((row) => {
+      const [name, price, quantity, promotion] = row
+        .split(',')
+        .map((value) => value.trim());
+      return new Product({
+        name,
+        price: Number(price),
+        quantity: Number(quantity),
+        promotion: promotion === 'null' ? null : promotion,
+      });
+    });
   }
 
   loadPromotions() {
     const filePath = path.resolve('public/promotions.md');
     const data = fs.readFileSync(filePath, 'utf-8').trim();
-    console.log(data);
+    const [header, ...rows] = data.split('\n');
+    return rows.map((row) => {
+      const [name, buy, get, startDate, endDate] = row
+        .split(',')
+        .map((value) => value.trim());
+      return new Promotion({
+        name,
+        buy: Number(buy),
+        get: Number(get),
+        startDate,
+        endDate,
+      });
+    });
+  }
+
+  updateStock(items) {
+    items.forEach(({ name, quantity }) => {
+      const product = this.products.find((p) => p.name === name);
+      product.reduceStock(quantity);
+    });
   }
 }
 
